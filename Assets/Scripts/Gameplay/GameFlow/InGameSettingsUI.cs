f2eea13a8d91ce3cb335295b6c15aea69c615dc0
msyariefh <msyariefhidayatulloh14@gmail.com>
2022-11-19 16:54:34 +0700

    Add Game Flow Modules

---
+++ Assets/Scripts/Gameplay/GameFlow/InGameSettingsUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GJ2022.Gameplay.GameFlow
+{
+    public class InGameSettingsUI : MonoBehaviour
+    {
+        
+    }
+
+}
