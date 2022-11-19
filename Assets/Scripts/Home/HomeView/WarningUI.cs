64fbadca765020f0c387f6d2fcaba245f979eb26
msyariefh <msyariefhidayatulloh14@gmail.com>
2022-11-19 11:04:08 +0700

    Add modules in Home layer

---
+++ Assets/Scripts/Home/HomeView/WarningUI.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using GJ2022.Global.SaveLoad;
+
+namespace GJ2022.Home.HomeView
+{
+    public class WarningUI : MonoBehaviour
+    {
+        [Header("Home Menu Buttons")]
+        [SerializeField] private Button _startButton;
+        [SerializeField] private Button _continueButton;
+        [SerializeField] private Button _closeButton;
+
+        [Header("Scene Manager")]
+        [SerializeField] private string _gameplaySceneName;
+
+        private void OnEnable()
+        {
+            RemoveAllButtonListeners();
+            _startButton.onClick.AddListener(OnStartButton);
+            _continueButton.onClick.AddListener(OnContinueButton);
+            _closeButton.onClick.AddListener(OnCloseButton);
+        }
+        private void OnDisable()
+        {
+            RemoveAllButtonListeners();
+        }
+
+        private void RemoveAllButtonListeners()
+        {
+            _startButton.onClick.RemoveAllListeners();
+            _continueButton.onClick.RemoveAllListeners();
+            _closeButton.onClick.RemoveAllListeners();
+        }
+
+        private void OnStartButton()
+        {
+            SaveSystem.Instance.ResetPlayerProgress();
+            SceneManager.LoadScene(_gameplaySceneName);
+        }
+        private void OnContinueButton()
+        {
+            SceneManager.LoadScene(_gameplaySceneName);
+        }
+        private void OnCloseButton()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
